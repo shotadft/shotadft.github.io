1bc2ba53f8d2cf783d01c525e379a8245926f0b9
shotadft <98450322+shotadft0780@users.noreply.github.com>
2023-07-26 19:14:42 +0900

    Create app_prc.js

---
+++ app/script/app_prc.js
@@ -0,0 +1,28 @@
+let use_language = window.navigator.language;
+var language_money = 'undefined';
+
+if (use_language === 'ja-JP' || use_language === 'ja')
+{
+	language_money = '\xA5';
+}
+else if (use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language === 'zh-CN' || use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' || use_language === 'zh-MO')
+{
+	language_money = '中' + '\xA5';
+}
+else
+{
+	language_money = '$';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+function Price(Divided, Price_n){
+    this.Price_n = Price_n;
+    this.Divided = Divided;
+    const replyDivided = document.getElementById(this.Divided);
+    
+    var paragraph = document.createElement('p');
+    paragraph.innerText = language_money + Price_n;
+    replyDivided.appendChild(paragraph);
+}
+    Price('OpenBrowserPrice', 0);
+});

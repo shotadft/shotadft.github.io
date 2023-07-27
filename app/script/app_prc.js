c98a74e4ad6232125507367d301663534ab2f64c
shotadft <98450322+shotadft0780@users.noreply.github.com>
2023-07-27 11:55:32 +0900

    Update app_prc.js

--- app/script/app_prc.js
+++ app/script/app_prc.js
@@ -7,11 +7,11 @@ if (use_language === 'ja-JP' || use_language === 'ja')
 }
 else if (use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language === 'zh-CN' || use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' || use_language === 'zh-MO')
 {
-	language_money = '中' + '\xA5';
+	language_money = '元';
 }
 else
 {
-	language_money = '$';
+	language_money = '\x24';
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -21,7 +21,14 @@ function Price(Divided, Price_n){
     const replyDivided = document.getElementById(this.Divided);
     
     var paragraph = document.createElement('p');
-    paragraph.innerText = language_money + Price_n;
+    if(use_language === 'zh' || use_language === 'zh-Hans' || use_language === 'zh-SG' || use_language === 'zh-CN' || use_language === 'zh-Hant' || use_language === 'zh-HK' || use_language === 'zh-TW' || use_language === 'zh-MO')
+    {
+	paragraph.innerText = Price_n + language_money;
+    }
+    else
+    {
+	paragraph.innerText = language_money + Price_n;
+    }
     replyDivided.appendChild(paragraph);
 }
 // ↓価格設定プログラム↓

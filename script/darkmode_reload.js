eaccbecb1df8703a0942a34c9dcd29fbdea0a779
Shota_dft(承太劇場) <98450322+shotadft0780@users.noreply.github.com>
2023-08-06 18:37:37 +0900

    Update darkmode_reload.js

--- script/darkmode_reload.js
+++ script/darkmode_reload.js
@@ -1,10 +1,11 @@
-setInterval(() => {
-if(window.matchMedia('(prefers-color-scheme: dark)').matches == true)
-{
-  location.reload();
-}
-else
-{
-  ;
-}
-}, 500);
+let isDarkmode = false;
+
+const darkmodeDOM = document;
+
+// オブザーバーを作る
+const observer = new IntersectionObserver(() => {
+  isDarkmode = (location.reload();)
+})
+
+// darkmodeDOMの監視開始
+observer.observe(darkmodeDOM);

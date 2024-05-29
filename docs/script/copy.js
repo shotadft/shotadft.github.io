e7ebe411790e31ae5a764b500df68a5fd26763e4
Shota_dft(承太劇場) <98450322+shotadft@users.noreply.github.com>
2024-05-29 19:45:09 +0900

    Fixed

--- docs/script/copy.js
+++ docs/script/copy.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const txt = document.getElementById('copyText');
-  const btn = document.getElementById('copy');
+  document.addEventListener('load', () => {
+    const txt = document.getElementById('copyText');
+    const btn = document.getElementById('copy');
 
-  btn.onclick = () => {
-    if (!navigator.clipboard) { console.error("このブラウザは\"navigator.clipboard\"に対応していません\nEdge, Chrome, Firefox等対応ブラウザを使用して下さい"); return false; }
-    if (typeof txt.value !== 'undefined') {
-      navigator.clipboard.writeText(txt.value).then(
-        () => { console.log("クリップボードにコピーしました") },
-        () => { console.log("コピーに失敗しました") }
-      );
+    btn.onclick = () => {
+      if (!navigator.clipboard) { console.error("このブラウザは\"navigator.clipboard\"に対応していません\nEdge, Chrome, Firefox等対応ブラウザを使用して下さい"); return false; }
+      if (txt.value) {
+        navigator.clipboard.writeText(txt.value).then(
+          () => { console.log("クリップボードにコピーしました") },
+          () => { console.log("コピーに失敗しました") }
+        );
+      }
     }
-  }
+  });
 });

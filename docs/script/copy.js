aa9aa247c635c8e0481a4c4f3410a386c02e4065
Shota_dft(承太劇場) <98450322+shotadft@users.noreply.github.com>
2024-06-02 21:12:58 +0900

    Fixed

--- docs/script/copy.js
+++ docs/script/copy.js
@@ -1,11 +1,10 @@
-$(document).ready(function () {
-  $('.copyButton').click(function () {
+$(document).ready(function() {
+  $('.copyButton').click(function() {
     const txt = document.getElementsByClassName('copyText');
-    let bTxt = $('.copyButton').txt();
     for (let i = 0, len = txt.length | 0; i < len; i = (i + 1) | 0) {
       console.log(`${txt[i].value}`);
       navigator.clipboard.writeText(`${txt[i].value}`).then(
-        () => { console.log("クリップボードにコピーしました"); $('.copyButton').txt('Copied!'); setTimeout(function () { $('.copyButton').txt(bTxt); }, 2000); },
+        () => { console.log("クリップボードにコピーしました"); },
         () => { console.error("コピーに失敗しました"); }
       );
     }

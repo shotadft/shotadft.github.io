ad79dbcab5fee2969d24b0dd5ca7a80203a723a3
shotadft <fukashtasabu@gmail.com>
2024-06-20 18:36:48 +0900

    Fixed

--- tools/tool/nds/Oimori/letter_gen/script/main.js
+++ tools/tool/nds/Oimori/letter_gen/script/main.js
@@ -28,11 +28,11 @@ $(function() {
 	});
 
 	$('input[name=intype]').change(function() {
-		if ($(`${this}#dec`).prop('checked')) {
+		if ($('input[name=intype]#dec').prop('checked') && $('input[name=intype]#hex').prop('checked') == false) {
 			$("#input_cnv").attr('type', 'number');
 			$("#input_cnv").attr('min', '0');
 			if ($("#input_cnv").attr('pattern')) {$("#input_cnv").removeAttr('pattern')}
-		} else if ($(`${this}#hex`).prop('checked')) {
+		} else if ($('input[name=intype]#hex').prop('checked') && $('input[name=intype]#dec').prop('checked') == false) {
 			$("#input_cnv").attr('type', 'text');
 			$("#input_cnv").attr('pattern', '^([a-f,A-F,0-9]|\[NUL [0-9]+\])+$');
 			if ($("#input_cnv").attr('min')) {$("#input_cnv").removeAttr('min')}
